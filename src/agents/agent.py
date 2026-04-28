d79110c50b4945e82a61fcc5527a8305e2671864
Alexey PC <aleshamarichev09@gmail.com>
2026-04-28 21:17:13 +0300

    agents add

--- src/agents/agent.py
+++ src/agents/agent.py
@@ -3,7 +3,7 @@ import torch, json
 
 
 class Agent:
-    def __init__(self, config: BitsAndBytesConfig, tools: list, model_name: str = 'Qwen/Qwen2.5-Coder-14B-Instruct', device='cuda'):
+    def __init__(self, config: BitsAndBytesConfig, tools: list = [], model_name: str = 'Qwen/Qwen2.5-Coder-14B-Instruct', device='cuda'):
         self.model = AutoModelForCausalLM.from_pretrained(
                     model_name,
                     quantization_config=config,
@@ -14,17 +14,28 @@ class Agent:
         self.model.eval()
 
 
+    def _edit_config(self, tools: list):
+        self.tools = tools
+
+
     def generate(self, text: str):
         message = [{'role': 'user',
                     'content': text
                     }
                    ]
-        message_with_template = self.tokenizer.apply_chat_template(
-            message,
-            add_generation_prompt=True,
-            tools=self.tools,
-            tokenize=False
-        )
+        if self.tools:
+            message_with_template = self.tokenizer.apply_chat_template(
+                message,
+                add_generation_prompt=True,
+                tools=self.tools,
+                tokenize=False
+            )
+        else:
+            message_with_template = self.tokenizer.apply_chat_template(
+                message,
+                add_generation_prompt=True,
+                tokenize=False
+            )
         tokens = self.tokenizer(message_with_template, return_tensors='pt').to(self.model.device)
         input_len = tokens.input_ids.shape[1]
         result = self.model.generate(
