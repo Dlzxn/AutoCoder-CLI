314ff0028db0782f62bf263809aabfff1f51fe9e
Alexey PC <aleshamarichev09@gmail.com>
2026-04-29 22:29:14 +0300

    refact state model

--- src/state/ProjectModel.py
+++ src/state/ProjectModel.py
@@ -6,6 +6,7 @@ from typing import List, Dict
 class Project(BaseModel):
     start_prompt: str
     project_plan: List[str] = Field(default_factory=list) # Здесь список задач TODO
+    description: str = Field(default_factory=str) #здесь краткое описание проекта для понимания
     project_state: Dict[str, str] = Field(default_factory=dict) # Здесь мапа: "название задачи" -> "статус (например, todo/done)"
-    review_state: str = Field(default_factory='') #здесь мы будем хранить вердикт ревьюшки
-    is_ready: bool = False
+    review_state: str = Field(default_factory=str) #здесь мы будем хранить вердикт ревьюшки
+    is_ready: bool = Field(default=False)
